591e07a71ccf9706411d2bd74f664a1e58696bf3
Deleav <a42006310@gmail.com>
2017-04-23 20:21:08 +0800

    add a todo list has add and delete function

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    {{ todo }}
+    <span @click="deleteItem">
+      X
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    'todo',
+    'index'
+  ],
+  methods: {
+    deleteItem() {
+      this.$emit( 'deleteItem' );
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  border: 1px solid;
+  border-radius: 5px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 5px;
+  padding: 5px;
+  width: 200px;
+}
+
+span {
+  float: right;
+}
+
+span:hover {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input v-model="todoText" @keyup.enter="add"/>
+    <button type="button" @click="add">ADD</button>
+    <ul>
+      <TodoItem v-for="(todo, index) in todos" :todo="todo" :key="index" @deleteItem="deleteItem( index )"></TodoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+
+export default {
+  data() {
+    return {
+      todos: [
+        'Do todo list',
+        'Do add function',
+        'Do remove function'
+      ],
+      todoText: ''
+    }
+  },
+  components: {
+    TodoItem
+  },
+  methods: {
+    add() {
+      if ( this.todoText.length > 0 ) {
+        this.todos.push( this.todoText );
+        this.todoText = '';
+      }
+    },
+    deleteItem( index ) {
+      this.todos.splice( index, 1 );
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+
+input {
+  width: 120px;
+}
+</style>
